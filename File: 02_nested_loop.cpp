88d379e02f475b071fc273c1c7271b153aad5928
Farza-web <farzanaakteromi93@gmail.com>
2026-05-11 00:26:34 +0600

    File: 02_nested_loop.cpp

---
+++ File: 02_nested_loop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    for(int i = 1; i <= 2; i++) {
+
+        cout << "Outer: " << i << endl;
+
+        for(int j = 1; j <= 2; j++) {
+
+            cout << "Inner: " << j << endl;
+        }
+    }
+
+    return 0;
+}
